e42201757373131f40b24355474b11e289fc010f
Andreas Grech <agnt666@gmail.com>
2023-09-01 18:07:30 +0200

    namespace

--- PhaserTypeDefExtractor/Program.cs
+++ PhaserTypeDefExtractor/Program.cs
@@ -6,55 +6,57 @@ using System.Text.RegularExpressions;
 using System.Collections.Concurrent;
 #endif
 
-enum JSDocOperand
+namespace PhaserTypeDefExtractor
 {
-    Typedef,
-    Callback
-}
-
-internal class Program
-{
-    // This regex captures JSDoc comments
-    private const string JSDOC_BLOCK_REGEX = @"/\*\*[^*]*\*+([^/*][^*]*\*+)*/"; 
-    private const string VALID_FILE_EXTENSIONS = "*.js";
-    private const string PHASER_DIRECTORY = @"C:\Users\Administrator\Documents\Github\phaser\src";
-    private const string OUTPUT_FILE_PATH = @"C:\Users\Administrator\Documents\jsBotArena\scripts\AllPhaserTypedefs.js";
-    private static readonly Encoding OUTPUT_FILE_ENCODING = Encoding.UTF8;
-
-    private static void Main()
+    enum JSDocOperand
     {
-        var typedefs = ExtractTypeDefs(PHASER_DIRECTORY, JSDocOperand.Typedef);
-        var callbacks = ExtractTypeDefs(PHASER_DIRECTORY, JSDocOperand.Callback);
-        
-        var allJSDocBlocks = new List<string>();
-        allJSDocBlocks.AddRange(typedefs);
-        allJSDocBlocks.AddRange(callbacks);
-        File.WriteAllLines(OUTPUT_FILE_PATH, allJSDocBlocks, OUTPUT_FILE_ENCODING);
+        Typedef,
+        Callback
     }
 
+    internal class Program
+    {
+        // This regex captures JSDoc comments
+        private const string JSDOC_BLOCK_REGEX = @"/\*\*[^*]*\*+([^/*][^*]*\*+)*/";
+        private const string VALID_FILE_EXTENSIONS = "*.js";
+        private const string PHASER_DIRECTORY = @"C:\Users\Administrator\Documents\Github\phaser\src";
+        private const string OUTPUT_FILE_PATH = @"C:\Users\Administrator\Documents\jsBotArena\scripts\AllPhaserTypedefs.js";
+        private static readonly Encoding OutputFileEncoding = Encoding.UTF8;
+
+        private static void Main()
+        {
+            var typedefs = ExtractTypeDefs(PHASER_DIRECTORY, JSDocOperand.Typedef);
+            var callbacks = ExtractTypeDefs(PHASER_DIRECTORY, JSDocOperand.Callback);
+
+            var allJSDocBlocks = new List<string>();
+            allJSDocBlocks.AddRange(typedefs);
+            allJSDocBlocks.AddRange(callbacks);
+            File.WriteAllLines(OUTPUT_FILE_PATH, allJSDocBlocks, OutputFileEncoding);
+        }
+
 #if MULTI_THREADED
-    private static ConcurrentBag<string> ExtractTypeDefs(string directoryPath, JSDocOperand jsDocOperand)
+        private static ConcurrentBag<string> ExtractTypeDefs(string directoryPath, JSDocOperand jsDocOperand)
 #else
     private static List<string> ExtractTypeDefs(string directoryPath, JSDocOperand jsDocOperand)
 #endif
-    {
-        var searchToken = jsDocOperand switch
         {
-            JSDocOperand.Typedef => "@typedef",
-            JSDocOperand.Callback => "@callback",
-            _ => throw new ArgumentOutOfRangeException(nameof(jsDocOperand), jsDocOperand, null)
-        };
-        
-        var jsFiles = Directory.GetFiles(directoryPath, VALID_FILE_EXTENSIONS, SearchOption.AllDirectories);
+            var searchToken = jsDocOperand switch
+            {
+                JSDocOperand.Typedef => "@typedef",
+                JSDocOperand.Callback => "@callback",
+                _ => throw new ArgumentOutOfRangeException(nameof(jsDocOperand), jsDocOperand, null)
+            };
+
+            var jsFiles = Directory.GetFiles(directoryPath, VALID_FILE_EXTENSIONS, SearchOption.AllDirectories);
 
 #if MULTI_THREADED
-        var allTypeDefs = new ConcurrentBag<string>();
-        Parallel.ForEach(jsFiles, jsFile =>
-        {
-            var content = File.ReadAllText(jsFile);
-            var jsDocBlocks = FindTypeDefsInContent(content, searchToken);
-            Parallel.ForEach(jsDocBlocks, jsDocBlock => { allTypeDefs.Add(jsDocBlock); });
-        });
+            var allTypeDefs = new ConcurrentBag<string>();
+            Parallel.ForEach(jsFiles, jsFile =>
+            {
+                var content = File.ReadAllText(jsFile);
+                var jsDocBlocks = FindTypeDefsInContent(content, searchToken);
+                Parallel.ForEach(jsDocBlocks, jsDocBlock => { allTypeDefs.Add(jsDocBlock); });
+            });
 #else
         var allTypeDefs = new List<string>();
         foreach (var jsFile in jsFiles)
@@ -65,22 +67,23 @@ internal class Program
         }
 #endif
 
-        return allTypeDefs;
-    }
-
-    private static IEnumerable<string> FindTypeDefsInContent(string content, string searchToken)
-    {
-        var typedefs = new List<string>();
-
-        var matches = Regex.Matches(content, JSDOC_BLOCK_REGEX);
-        foreach (Match match in matches)
-        {
-            if (match.Value.Contains(searchToken))
-            {
-                typedefs.Add(match.Value);
-            }
+            return allTypeDefs;
         }
 
-        return typedefs;
+        private static IEnumerable<string> FindTypeDefsInContent(string content, string searchToken)
+        {
+            var typedefs = new List<string>();
+
+            var matches = Regex.Matches(content, JSDOC_BLOCK_REGEX);
+            foreach (Match match in matches)
+            {
+                if (match.Value.Contains(searchToken))
+                {
+                    typedefs.Add(match.Value);
+                }
+            }
+
+            return typedefs;
+        }
     }
 }
